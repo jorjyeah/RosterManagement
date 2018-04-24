c309e96e6c036d1b71cecdea5f7985e4e477685a
gnmarvin <nathaneilmarvin96@gmail.com>
2018-04-24 19:13:21 +0900

    test

--- app/src/main/java/id/ac/umn/rostermanagement/AddEventActivity.java
+++ app/src/main/java/id/ac/umn/rostermanagement/AddEventActivity.java
@@ -3,8 +3,14 @@ package id.ac.umn.rostermanagement;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 
 public class AddEventActivity extends AppCompatActivity {
 
@@ -22,5 +28,27 @@ public class AddEventActivity extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.job);
         listView.setAdapter(adapter);
+    }
+
+    public void onButtonShowPopupWindowClick(View view) {
+        RelativeLayout addEvent = (RelativeLayout) findViewById(R.id.add_event_layout);
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.date_picker, null);
+
+        int width = RelativeLayout.LayoutParams.WRAP_CONTENT;
+        int height = RelativeLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true;
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        popupWindow.showAtLocation(addEvent, Gravity.CENTER, 0 ,0);
+
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 }
